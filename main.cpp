6a3fd7d9fa11a02fd236f6d09ecbb7f3089e2334
Yash Ramesh Kumar <yashk@yashk.dev>
2024-10-17 20:48:45 +0530

    first commit

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <pybind11/pybind11.h>
+
+
+namespace py = pybind11;
+
+void something() {
+    std::cout << "HEllo world something\n" << std::endl;
+    // return;
+}
+
+float addFloat(float a, float b) {
+    std::cout << a+b << "Hello world" << std::endl;
+    return a+b;
+}
+
+PYBIND11_MODULE(pathfinder_mod, handle) {
+    handle.doc() = "Some shitty documentation for this purpose";
+    handle.def("cpp_addFloat", &addFloat);
+}
+
+
