e7a05c1c7b0f546af58090f1c0014920ea16f960
devyk100 <devyk100@gmail.com>
2024-10-30 00:33:56 +0530

    finally done with the motor driver part

--- src/sensor-testing/motor_driver_l298n.py
+++ src/sensor-testing/motor_driver_l298n.py
@@ -3,107 +3,56 @@
 
 import RPi.GPIO as GPIO
 from time import sleep
+
 GPIO.setmode(GPIO.BCM)
+
+
 class Motor:
-    def __init__(self, inlet1, inlet2, enable, digital_frequency):
+    def __init__(self, inlet1, inlet2, enable, digital_frequency, speed_offset):
         self.inlet1 = inlet1
         self.inlet2 = inlet2
         self.enable = enable
+        self.speed_offset = speed_offset
         self.digital_frequency = digital_frequency
         GPIO.setup(self.inlet1, GPIO.OUT)
         GPIO.setup(self.inlet2, GPIO.OUT)
-        GPIO.setup(self.enable,GPIO.OUT)
-        GPIO.output(self.inlet1,GPIO.LOW)
-        GPIO.output(self.inlet2,GPIO.LOW)
+        GPIO.setup(self.enable, GPIO.OUT)
+        GPIO.output(self.inlet1, GPIO.LOW)
+        GPIO.output(self.inlet2, GPIO.LOW)
         self.pwm = GPIO.PWM(self.enable, self.digital_frequency)
-        self.pwm.start(25)
-    def motorStop(self):
-        pass
-    def moveForward(self):
-        pass
+        self.pwm.start(25+speed_offset)
 
-    def moveReverse(self):
-        pass
+    def stop_motor(self):
+        GPIO.output(self.inlet1, GPIO.LOW)
+        GPIO.output(self.inlet2, GPIO.LOW)
 
-    def setMotorSpeed(self):
-        pass
-in1 = 23
-in2 = 24
-en = 25
-temp1=1
+    def move_forward(self):
+        GPIO.output(self.inlet1, GPIO.HIGH)
+        GPIO.output(self.inlet2, GPIO.LOW)
 
-GPIO.setup(in1,GPIO.OUT)
-GPIO.setup(in2,GPIO.OUT)
-GPIO.setup(en,GPIO.OUT)
-GPIO.output(in1,GPIO.LOW)
-GPIO.output(in2,GPIO.LOW)
-p=GPIO.PWM(en,100)
+    def move_reverse(self):
+        GPIO.output(self.inlet1, GPIO.LOW)
+        GPIO.output(self.inlet2, GPIO.HIGH)
 
-p.start(25)
-print("\n")
-print("The default speed & direction of motor is LOW & Forward.....")
-print("r-run s-stop f-forward b-backward l-low m-medium h-high e-exit")
-print("\n")
+    def set_motor_speed(self, speed):
+        self.pwm.ChangeDutyCycle(speed+self.speed_offset)
+        return True
+try:
+    motor1 = Motor(23, 24, 25, 100, 7)
+    motor2 = Motor(27, 17, 22, 100, 0)
 
-while(1):
+    motor1.move_forward()
+    motor2.move_forward()
+    sleep(10)
 
-    x=input()
+    motor1.move_reverse()
+    motor2.move_reverse()
 
-    if x=='r':
-        print("run")
-        if(temp1==1):
-            GPIO.output(in1,GPIO.HIGH)
-            GPIO.output(in2,GPIO.LOW)
-            print("forward")
-            x='z'
-        else:
-            GPIO.output(in1,GPIO.LOW)
-            GPIO.output(in2,GPIO.HIGH)
-            print("backward")
-            x='z'
+    sleep(10)
 
+    motor1.stop_motor()
+    motor2.stop_motor()
 
-    elif x=='s':
-        print("stop")
-        GPIO.output(in1,GPIO.LOW)
-        GPIO.output(in2,GPIO.LOW)
-        x='z'
-
-    elif x=='f':
-        print("forward")
-        GPIO.output(in1,GPIO.HIGH)
-        GPIO.output(in2,GPIO.LOW)
-        temp1=1
-        x='z'
-
-    elif x=='b':
-        print("backward")
-        GPIO.output(in1,GPIO.LOW)
-        GPIO.output(in2,GPIO.HIGH)
-        temp1=0
-        x='z'
-
-    elif x=='l':
-        print("low")
-        p.ChangeDutyCycle(25)
-        x='z'
-
-    elif x=='m':
-        print("medium")
-        p.ChangeDutyCycle(50)
-        x='z'
-
-    elif x=='h':
-        print("high")
-        p.ChangeDutyCycle(75)
-        x='z'
-
-
-    elif x=='e':
-        GPIO.cleanup()
-        print("GPIO Clean up")
-        break
-
-    else:
-        print("<<<  wrong data  >>>")
-        print("please enter the defined data to continue.....")
+    GPIO.cleanup()
+except KeyboardInterrupt:
+    GPIO.cleanup()
