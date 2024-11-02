60de479334ffacbc4b1ad0f6bc6209362b58e13f
devyk100 <devyk100@gmail.com>
2024-11-02 17:12:42 +0530

    trying out something

--- main.py
+++ main.py
@@ -37,11 +37,11 @@ number_thread = threading.Thread(target=staller)
 number_thread.start()
 
 while is_on:
-    if state.getAngleDegrees() > 0.5:
+    if state.getAngleDegrees() > 0.3:
         print("burst left")
         leftMotor.set_motor_speed(75)
         rightMotor.set_motor_speed(50)
-    elif state.getAngleDegrees() < -0.5:
+    elif state.getAngleDegrees() < -0.3:
         print("burst right")
         leftMotor.set_motor_speed(50)
         rightMotor.set_motor_speed(75)
