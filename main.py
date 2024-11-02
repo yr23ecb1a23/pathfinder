41b7038f8e8055017b1849200c9366795276b729
devyk100 <devyk100@gmail.com>
2024-11-02 16:45:36 +0530

    trying out something

--- main.py
+++ main.py
@@ -16,9 +16,9 @@ if module_path not in sys.path:
 rightMotor = motor.Motor(24, 23, 25, 100, 0)  # left motor
 leftMotor = motor.Motor(17, 27, 22, 100, 0)
 state = robot_state.RobotState()
-#rightMotor.move_forward()
+rightMotor.move_forward()
 rightMotor.set_motor_speed(50)
-#leftMotor.move_forward()
+leftMotor.move_forward()
 leftMotor.set_motor_speed(50)
 
 # for i in range(10):
@@ -26,7 +26,17 @@ leftMotor.set_motor_speed(50)
 #     sleep(0.34)
 
 while True:
-    print(state.getAngleDegrees())
+    if(state.getAngleDegrees() > 1):
+        print("burst left")
+        leftMotor.set_motor_speed(70)
+        rightMotor.set_motor_speed(0)
+    elif(state.getAngleDegrees() < -1):
+        print("burst right")
+        leftMotor.set_motor_speed(0)
+        rightMotor.set_motor_speed(70)
+    else:
+        rightMotor.set_motor_speed(50)
+        leftMotor.set_motor_speed(50)
 sleep(20)
 rightMotor.stop_motor()
 leftMotor.stop_motor()
