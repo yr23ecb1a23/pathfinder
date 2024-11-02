280915dad8132b49a8bbb83d4be4340cf4d258f4
devyk100 <devyk100@gmail.com>
2024-11-02 13:39:41 +0530

    changes 2

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ if module_path not in sys.path:
 #print(cpp_addFloat(102,123))
 
 state = robot_state.RobotState()
-for i in range(0,1000):
+for i in range(10):
     print(state.getAngle())
     sleep(0.34)
 
