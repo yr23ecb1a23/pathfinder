33bda60c4e1e72d0780de4a4cff7a1cc5104c305
devyk100 <devyk100@gmail.com>
2024-11-02 17:11:02 +0530

    soemthing

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ is_on = True
 
 def staller():
     global is_on
-    sleep(1)
+    sleep(0.6)
     is_on = False
 
 rightMotor = motor.Motor(24, 23, 25, 100, 0)  # left motor
@@ -37,11 +37,11 @@ number_thread = threading.Thread(target=staller)
 number_thread.start()
 
 while is_on:
-    if state.getAngleDegrees() > 0.6:
+    if state.getAngleDegrees() > 0.5:
         print("burst left")
         leftMotor.set_motor_speed(75)
         rightMotor.set_motor_speed(50)
-    elif state.getAngleDegrees() < -0.6:
+    elif state.getAngleDegrees() < -0.5:
         print("burst right")
         leftMotor.set_motor_speed(50)
         rightMotor.set_motor_speed(75)
