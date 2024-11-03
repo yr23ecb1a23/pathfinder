1cb49076ae2df438073cb99464057e450c93de05
devyk100 <devyk100@gmail.com>
2024-11-03 23:45:50 +0530

    sometjing

--- src/backend.py
+++ src/backend.py
@@ -1,11 +1,10 @@
 from flask import Flask, request, jsonify, send_from_directory
-import threading
 
 app = Flask(__name__)
 
 # Default destination
 current_destination = -1
-
+delivery_done = False  # New state to track delivery status
 
 @app.route('/set_destination', methods=['POST'])
 def set_destination():
@@ -19,20 +18,24 @@ def set_destination():
     else:
         return jsonify({'message': 'Invalid destination. Choose between 1 and 4.'}), 400
 
-
 @app.route('/get_destination', methods=['GET'])
 def get_destination():
     return jsonify({'current_destination': current_destination}), 200
 
+@app.route('/delivery_done', methods=['POST'])
+def delivery_done_route():
+    global delivery_done
+    delivery_done = True  # Set delivery state to done
+    return jsonify({'message': 'Delivery marked as done!'}), 200
+
+@app.route('/get_delivery_done', methods=['GET'])
+def get_delivery_done_route():
+    global delivery_done
+    return jsonify({'delivery': delivery_done})
 
 @app.route('/')
 def serve_index():
     return send_from_directory('.', 'index.html')
 
-
-def run_backend():
-    app.run(host='0.0.0.0', port=5000)
-
 if __name__ == '__main__':
-    pass
-    # app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000)
