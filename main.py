9da2e77d2c28e29a0470532738367bc5355e09a2
devyk100 <devyk100@gmail.com>
2024-11-03 20:36:46 +0530

    done with the app

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-from src import test, robot_state, motor
+from src import test, robot_state, motor, backend
 import os
 import sys
 import threading
+import requests
 from time import sleep
 import time
 import RPi.GPIO as GPIO
+
 module_path = os.path.abspath("./build")
 test.someRandomTest()
 if module_path not in sys.path:
@@ -20,6 +22,8 @@ GPIO.setup(TRIG, GPIO.OUT)
 GPIO.setup(ECHO, GPIO.IN)
 
 is_path_blocked = False
+
+
 def staller():
     global is_on
     is_on = True
@@ -32,6 +36,10 @@ def staller():
 
 
 us_lock = threading.Lock()
+
+def backend_thread():
+    backend.app.run(host='0.0.0.0', port=5000)
+
 def ultrasonic_thread():
     while is_thread_on:
         global is_path_blocked
@@ -64,6 +72,27 @@ def ultrasonic_thread():
 
         print(is_path_blocked)
 
+current_destination = -1
+def poll_backend(url):
+    global current_destination
+    while True:
+        try:
+            response = requests.get(url)
+            data = response.json()
+
+            current_destination = data.get('current_destination', -1)
+            print(f"Current destination: {current_destination}")
+
+            if current_destination != -1:
+                print("Destination has been set, exiting polling.")
+                break
+            print("Polling again")
+            time.sleep(1)  # Wait before polling again
+        except requests.exceptions.RequestException as e:
+            print(f"Error while polling: {e}")
+            time.sleep(2)  # Wait a bit longer on error
+backend_url = 'http://localhost:5000/get_destination'  # Use localhost for local testing
+poll_backend(backend_url)
 
 rightMotor = motor.Motor(24, 23, 25, 100, 0)  # left motor
 leftMotor = motor.Motor(17, 27, 22, 100, 0)
@@ -73,13 +102,15 @@ rightMotor.set_motor_speed(50)
 leftMotor.move_forward()
 leftMotor.set_motor_speed(50)
 
+backend_thread_obj = threading.Thread(target=backend_thread)
+backend_thread_obj.start()
 obstacle_detection_thread = threading.Thread(target=ultrasonic_thread)
 obstacle_detection_thread.start()
 
 iterations = 0
 
 inp = True
-while False:
+while inp:
     number_thread = threading.Thread(target=staller)
     leftMotor.move_forward()
     rightMotor.move_forward()
@@ -89,11 +120,11 @@ while False:
         angle = state.getAngleDegrees()
         print(angle)
         if angle > 0.17:
-            leftMotor.set_motor_speed(100-20)
-            rightMotor.set_motor_speed(50-20)
+            leftMotor.set_motor_speed(100 - 20)
+            rightMotor.set_motor_speed(50 - 20)
         elif angle < -0.17:
-            leftMotor.set_motor_speed(50-20)
-            rightMotor.set_motor_speed(100-20)
+            leftMotor.set_motor_speed(50 - 20)
+            rightMotor.set_motor_speed(100 - 20)
         else:
             rightMotor.set_motor_speed(50)
             leftMotor.set_motor_speed(50)
@@ -110,7 +141,7 @@ while False:
     leftMotor.stop_motor()
     iterations += 1
     sleep(1)
-    if iterations > 0:
+    if iterations > current_destination-1:
         print("exiting")
         inp = False
         break
@@ -119,7 +150,7 @@ while False:
         is_on = True
 
 u_turn_done = False
-
+# u turn
 leftMotor.set_motor_speed(100)
 rightMotor.set_motor_speed(70)
 leftMotor.move_reverse()
@@ -127,16 +158,55 @@ rightMotor.move_forward()
 sleep(0.4)
 while not u_turn_done:
     angle = state.getAngleDegrees()
-    if angle > -176:
+    if angle < 176:
         leftMotor.move_reverse()
         rightMotor.move_forward()
-    elif angle < -186:
+    elif angle > 186:
         leftMotor.move_forward()
         rightMotor.move_reverse()
     else:
         break
 
+while inp:
+    number_thread = threading.Thread(target=staller)
+    leftMotor.move_forward()
+    rightMotor.move_forward()
+    number_thread.start()
+
+    while is_on:
+        angle = state.getAngleDegrees()
+        print(angle)
+        if angle > 0.17:
+            leftMotor.set_motor_speed(100 - 20)
+            rightMotor.set_motor_speed(50 - 20)
+        elif angle < -0.17:
+            leftMotor.set_motor_speed(50 - 20)
+            rightMotor.set_motor_speed(100 - 20)
+        else:
+            rightMotor.set_motor_speed(50)
+            leftMotor.set_motor_speed(50)
+        with us_lock:
+            if is_path_blocked:
+                rightMotor.stop_motor()
+                leftMotor.stop_motor()
+                sleep(0.2)
+            else:
+                rightMotor.move_forward()
+                leftMotor.move_forward()
+    number_thread.join()
+    rightMotor.stop_motor()
+    leftMotor.stop_motor()
+    iterations += 1
+    sleep(1)
+    if iterations > current_destination-1:
+        print("exiting")
+        inp = False
+        break
+    else:
+        inp = True
+        is_on = True
 
 is_thread_on = False
 obstacle_detection_thread.join()
 state.destroy()
+backend_thread_obj.join()
---
+++ src/backend.py
@@ -0,0 +1,29 @@
+from flask import Flask, request, jsonify, send_from_directory
+
+app = Flask(__name__)
+
+# Default destination
+current_destination = -1
+
+@app.route('/set_destination', methods=['POST'])
+def set_destination():
+    global current_destination
+    data = request.json
+    destination = data.get('destination')
+
+    if destination in [1, 2, 3, 4]:
+        current_destination = destination
+        return jsonify({'message': 'Destination set successfully', 'destination': current_destination}), 200
+    else:
+        return jsonify({'message': 'Invalid destination. Choose between 1 and 4.'}), 400
+
+@app.route('/get_destination', methods=['GET'])
+def get_destination():
+    return jsonify({'current_destination': current_destination}), 200
+
+@app.route('/')
+def serve_index():
+    return send_from_directory('.', 'index.html')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
