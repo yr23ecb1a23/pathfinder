f04fd949f70b9687e03cc04a368ea4fc4c619d36
devyk100 <devyk100@gmail.com>
2024-11-03 15:51:01 +0530

    someting

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ import os
 import sys
 import threading
 from time import sleep
-
+import time
+import RPi.GPIO as GPIO
 module_path = os.path.abspath("./build")
 test.someRandomTest()
 if module_path not in sys.path:
@@ -11,14 +12,50 @@ if module_path not in sys.path:
 
 is_on = True
 
+TRIG = 4
+ECHO = 5
 
+# Set up the GPIO pins
+GPIO.setup(TRIG, GPIO.OUT)
+GPIO.setup(ECHO, GPIO.IN)
 
+is_path_blocked = False
 def staller():
     global is_on
-    is_on = True
-    print("inside")
-    sleep(1.6)
+    for i in range(0, 160):
+        is_on = True
+        sleep(0.01)
     is_on = False
+
+
+
+def ultrasonic_thread():
+    while True:
+        global is_path_blocked
+
+        # Set TRIG to HIGH for 10 microseconds
+        GPIO.output(TRIG, True)
+        sleep(0.00001)
+        GPIO.output(TRIG, False)
+
+        # Record the start time
+        start_time = time.time()
+        while GPIO.input(ECHO) == 0:
+            start_time = time.time()
+
+        # Record the arrival time
+        stop_time = time.time()
+        while GPIO.input(ECHO) == 1:
+            stop_time = time.time()
+
+        # Calculate the distance
+        time_elapsed = stop_time - start_time
+        distance = (time_elapsed * 34300) / 2  # Distance in cm
+
+        if distance < 20:
+            is_path_blocked = True
+        else:
+            is_path_blocked = False
 
 
 rightMotor = motor.Motor(24, 23, 25, 100, 0)  # left motor
@@ -41,6 +78,8 @@ while inp:
     while is_on:
         angle = state.getAngleDegrees()
         print(angle, state.getDisp())
+
+
         if angle > 0.17:
             leftMotor.set_motor_speed(100-20)
             rightMotor.set_motor_speed(50-20)
@@ -51,11 +90,14 @@ while inp:
             rightMotor.set_motor_speed(50)
             leftMotor.set_motor_speed(50)
 
+        if is_path_blocked:
+            rightMotor.stop_motor()
+            leftMotor.stop_motor()
     number_thread.join()
     rightMotor.stop_motor()
     leftMotor.stop_motor()
     iterations += 1
-    sleep(0.2)
+    sleep(1)
     if iterations > 0:
         print("exiting")
         inp = False
